b5cdcf5fc3312d85ea20486de19a4fe9184b107e
suman1994 <sumanarora1994@yahoo.com>
2016-09-08 11:19:49 +0530

    Chat

--- register_action.php
+++ register_action.php
@@ -1,16 +1,16 @@
 <?php 
-include "connection.php";
+session_start();
+require_once('connection.php');
 
 	if(isset($_POST['submit']))
 	{ 
-		$fname = $_POST['first_name']; 
-		$lname = $_POST['last_name']; 
-		$email = $_POST['email']; 
-		$password = $_POST['password']; 
+		$fname = isset($_POST['first_name']) ? trim($_POST['first_name']) : ""; 
+		$lname = isset($_POST['last_name']) ? trim($_POST['last_name']) : ""; 		$email = isset($_POST['email']) ? trim($_POST['email']) : "";
+		$password = isset($_POST['password']) ? trim($_POST['password']) : ""; 
     
     $sql = "INSERT INTO users (first_name, last_name, email, password)
 	VALUES ('$fname', '$lname', '$email', '$password')";
 	$conn->exec($sql);
     echo "New record inserted sucessfully";
     }
-?>
+?>	
